e626f5a9761eca02578432a2beb5c0f491429e78
mdshahzad <01.mdshahzad@gmail.com>
2025-09-16 00:47:03 +0530

    Implement email functionality using nodemailer and update package dependencies

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,31 +1,34 @@
-const nodemailer = require('nodemailer');
+import nodemailer from "nodemailer";
 
-async function sendMail({ to, subject, text, html }) {
-    // Configure the transporter
-    const transporter = nodemailer.createTransport({
-        service: 'gmail', // or another email service
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+async function sendMail(to, subject, text, html) {
+  if (!to) {
+    throw new Error("No recipients defined");
+  }
+  // Configure the transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // or another email service
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
-    // Email options
-    const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-        html,
-    };
+  // Email options
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+    html,
+  };
 
-    // Send the email
-    try {
-        const info = await transporter.sendMail(mailOptions);
-        return info;
-    } catch (error) {
-        throw error;
-    }
+  // Send the email
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return info;
+  } catch (error) {
+    throw error;
+  }
 }
 
-module.exports = sendMail;
+export default sendMail;
